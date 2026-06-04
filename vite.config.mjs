de04a14647df2a80676c80d84375acc6bd0b9935
BanavathS45 <prashanthr803@gmail.com>
2026-06-04 18:54:12 +0530

    RemovedPWA

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { VitePWA } from 'vite-plugin-pwa'
+
 import path from 'node:path'
 import autoprefixer from 'autoprefixer'
 import { COLORS } from './src/Constant/Themes'
@@ -37,34 +37,7 @@ export default defineConfig(() => {
       },
     },
     plugins: [
-      react(),
-      VitePWA({
-        registerType: 'autoUpdate',
-        manifest: {
-          name: 'Clinic Admin',
-          short_name: 'Clinic Admin',
-          description: 'PhysioCare Clinic Administration Panel',
-          theme_color: COLORS.sideColor,
-          background_color: '#ffffff',
-          display: 'standalone',
-          start_url: '/',
-          icons: [
-            {
-              src: './src/assets/images/DermaCare.png',
-              sizes: '192x192',
-              type: 'image/png'
-            },
-            {
-              src: './src/assets/images/DermaCare.png',
-              sizes: '512x512',
-              type: 'image/png'
-            }
-          ]
-        },
-        devOptions: {
-          enabled: true
-        }
-      })
+      react()
     ],
     resolve: {
       alias: [
@@ -79,8 +52,8 @@ export default defineConfig(() => {
       host: '0.0.0.0',  // Ensure the server listens on all interfaces
       port: 3000,
       proxy: {
-        // https://vitejs.dev/config/server-options.html
+          // https://vitejs.dev/config/server-options.html
+        },
       },
-    },
-  }
-})
+    }
+  })
